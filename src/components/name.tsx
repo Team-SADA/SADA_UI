f985b1820e834f1fbb033cfa7dd1cd8d515d8ab1
piz2a <43025513+piz2a@users.noreply.github.com>
2024-07-11 21:13:02 +0900

    내꺼야

--- src/components/name.tsx
+++ src/components/name.tsx
@@ -4,14 +4,14 @@ import classes from "./name.module.scss";
 const nameChars = [
     "가감갑강건겸경고공곽관교구국권",
     "규균금기김나난남노다도동라랑로",
-    "록류륜리린림마명묵문미민박방배",
+    "록류륜리린림마명무묵문미민박방배",
     "백범변병보빈빛삭상서석선성세소",
     "손솔송수순승시신아안양엘연열영",
     "예오옥완요용우욱웅원유윤율은의",
     "이인일임장재전정제조종주준지진",
     "차찬창채천철청최태택하한함해허",
     "헌혁현형혜호홍환황효후훈휘흔희",
-].join("").split("");  // 135 = 15 * 9 characters
+].join("").split("");  // 135 = 15 * 9 characters + 1
 
 
 
@@ -33,10 +33,10 @@ export default function Name() {
             </div>
             <div className={classes.basket}>
                 <img src={"./basket.png"} alt="basket" width={120} height={90}/>
-                <button type="button" onClick={() => {}}>
+                <button type="button" className="button1" onClick={() => {}}>
                     입력
                 </button>
-                <button type="button" onClick={() => {}}>
+                <button type="button" className="button2" onClick={() => {}}>
                     버리기
                 </button>
             </div>
