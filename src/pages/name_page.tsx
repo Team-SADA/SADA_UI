d97532fa95b9050712da7d3091ef2d3189734377
piz2a <43025513+piz2a@users.noreply.github.com>
2024-07-12 10:42:18 +0900

    양식

--- src/pages/name_page.tsx
+++ src/pages/name_page.tsx
@@ -1,5 +1,6 @@
 import { useNavigate } from "react-router";
 import classes from "./name_page.module.scss";
+import {useEffect, useState} from "react";
 
 const nameChars = [
   "가감갑강건겸경고공곽관교구국권",
@@ -11,44 +12,91 @@ const nameChars = [
   "이인일임장재전정제조종주준지진",
   "차찬창채천철청최태택하한함해허",
   "헌혁현형혜호홍환황효후훈휘흔희",
-]
-  .join("")
-  .split(""); // 135 = 15 * 9 characters + 1
+].join("").split(""); // 135 = 15 * 9 characters + 1
 
-export default function NamePage() {
-  const navigate = useNavigate();
 
-  const submitHandler = () => {
-    navigate("/birthday");
-  };
+function shuffle(array: any[]) {
+    let currentIndex = array.length;
+    while (currentIndex !== 0) {
+        let randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+        [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+    }
+    return array;
+}
 
-  return (
-    <main>
-      <h2>여러분의 이름에 포함된 글자를 바구니에 담아보아요</h2>
-      <div className={classes.description}>
-        같은 글자는 두 번 다시 나타나지 않아요. 글자를 놓쳤다면 처음부터!!!
-      </div>
-      <div className={classes.nameTable}>
-        {nameChars.map((char) => (
-          <span className={classes.line}>
-            <button type="button" onMouseEnter={() => {}} key={char}>
-              {char}
+interface Block {
+    char: string;
+    left: number;
+}
+
+
+
+export default function Name() {
+    const navigate = useNavigate();
+
+    const submitHandler = () => {
+        navigate("/birthday");
+    };
+
+    const [mouseX, setMouseX] = useState(0);
+    const charQueue = shuffle(nameChars);
+    const [newCharIndex, setNewCharIndex] = useState(10);
+
+    const createNewBlock = (): Block => {
+        if (newCharIndex >= charQueue.length)
+            return {char: "", left: 0};
+        const data = charQueue[newCharIndex];
+        setNewCharIndex(newCharIndex + 1);
+        return {char: data, left: Math.random() * window.innerWidth};
+    };
+
+    const [blockList, setBlockList] = useState<Block[]>(
+        charQueue.slice(0, 10).map(char => {return {char: char, left: Math.random() * window.innerWidth}})
+    );
+
+    useEffect(() => {
+        document.addEventListener('mousemove', event => setMouseX(event.clientX));
+    }, []);
+
+    useEffect(() => {
+        setTimeout(() => {
+            const newBlock = createNewBlock();
+            if (newBlock.char !== "")
+                setBlockList([...blockList.slice(1, blockList.length), newBlock]);
+        }, 100);
+    }, [blockList]);
+
+    return (
+        <section>
+            <h2>이름을 입력하세요</h2><input type="text" value={"123"}/>
+            <div className={classes.description}>
+                바구니에 글자를 담아보아요.
+                글자 순서는 상관없어요.
+                같은 글자는 두 번 다시 나타나지 않아요.
+                글자를 놓쳤다면... 안타까운 거죠
+            </div>
+            <div className={classes.nameTable}>
+                {blockList.map(block => (
+                    <span className={classes.line} style={{left: block.left}}>
+                        {block.char}
+                    </span>
+                ))}
+            </div>
+            <div className={classes.basket} style={{left: mouseX - 60}}>
+                <img src={"./basket.png"} alt="basket"/>
+                <button type="button" className={classes.button1} onClick={() => {
+                }}>
+                    입력
+                </button>
+                <button type="button" className={classes.button2} onClick={() => {
+                }}>
+                    버리기
+                </button>
+            </div>
+            <button className="btn-flat" onClick={submitHandler}>
+                넘어가기
             </button>
-          </span>
-        ))}
-      </div>
-      <div className={classes.basket}>
-        <img src="./basket.png" alt="basket" width={120} height={90} />
-        <button type="button" className="button1" onClick={() => {}}>
-          입력
-        </button>
-        <button type="button" className="button2" onClick={() => {}}>
-          버리기
-        </button>
-      </div>
-      <button className="btn-flat" onClick={submitHandler}>
-        넘어가기
-      </button>
-    </main>
-  );
+        </section>
+    )
 }
