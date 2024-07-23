e2f9d132cb2f549f950771162b5ebccca70a2d83
hyper1423 <backup8822@gmail.com>
2024-07-23 21:36:17 +0900

    Implement Dorm room number input & Install React-Unity-WebGL

--- src/pages/dorm_room_page.tsx
+++ src/pages/dorm_room_page.tsx
@@ -1,4 +1,6 @@
 import { useNavigate } from "react-router";
+import classes from "./dorm_room_page.module.scss";
+import { Unity, useUnityContext } from "react-unity-webgl";
 
 const DormRoomPage = () => {
   const navigate = useNavigate();
@@ -6,12 +8,20 @@ const DormRoomPage = () => {
   const onSubmit = () => {
     navigate("/phone-number");
   };
+  
+  const { unityProvider } = useUnityContext({
+    loaderUrl: "dorm_unity_build/__build.loader.js",
+    dataUrl: "dorm_unity_build/webgl.data",
+    frameworkUrl: "dorm_unity_build/build.framework.js",
+    codeUrl: "dorm_unity_build/build.wasm"
+  });
 
   return (
     <main>
       기숙사 호실
+      <Unity className={classes.unity} unityProvider={unityProvider} />
       <button className="btn-flat" onClick={onSubmit}>
-        asdfjaskdlfjaskfasjdfklasd
+        asdfasdfasdfsadf
       </button>
     </main>
   );
